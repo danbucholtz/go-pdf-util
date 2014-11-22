e4bebd95a49b145ea124bb953c4fc33facf5b8f3
Dan Bucholtz <dan.bucholtz@gmail.com>
2014-11-22 01:56:41 -0600

    test

--- main.go
+++ main.go
@@ -50,43 +50,54 @@ func main() {
                 // w00t, we read in the image.  Close the reader at some point in the future since we're done with it
                 defer reader.Close()
 
-    			fmt.Printf("%s %d %d\n", args[i], im.Width, im.Height)
+    			fmt.Printf("Page #%d ... START\n", i + 1)
 
                 // use floats instead of ints
                 height := float64(im.Height)
                 width := float64(im.Width)
 
-                dpi := 200.0
+                dpi := 96.0
+
+                fmt.Printf("Image Height: %f   Image Width: %f\n", height, width)
 
                 totalHorizontalPixels := dpi * pageWidth
                 totalVerticalPixels := dpi * pageHeight
+
+                fmt.Printf("totalHorizontalPixels: %f   totalVerticalPixels: %f\n", totalHorizontalPixels, totalVerticalPixels)
 
                 isPortraitImage := height >= width
 
                 // resize the image if it is bigger than the page at a given dpi
 
                 if isPortraitImage {
-
+                    fmt.Printf("Image is Portrait\n")
                     if height > totalVerticalPixels {
 
                         scaleFactor := totalVerticalPixels/height
 
                         height = height * scaleFactor
                         width = width * scaleFactor
+
+                        fmt.Printf("Scaling necessary...   scaleFactor: %f   height: %f   width %f\n", scaleFactor, height, width)
                     }
                 } else{
+                    fmt.Printf("Image is Landscape\n")
                     if width > totalHorizontalPixels {
 
                         scaleFactor := totalHorizontalPixels/width
 
                         height = height * scaleFactor
                         width = width * scaleFactor
+
+                        fmt.Printf("Scaling necessary...   scaleFactor: %f   height: %f   width %f\n", scaleFactor, height, width)
                     }
                 }
                 
                 // figure out margins
                 widthDifferenceInPixels := totalHorizontalPixels - width
                 heightDifferenceInPixels := totalVerticalPixels - height
+
+                fmt.Printf("widthDifferenceInPixels: %f   heightDifferenceInPixels: %f\n", widthDifferenceInPixels, heightDifferenceInPixels )
 
                 xMargin := 0.0
                 yMargin := 0.0
@@ -100,23 +111,29 @@ func main() {
                 }
 
                 // okay, cool, we have our margins and the proper image dimensions, so now try to render the pdf page
-                
+                _ = xMargin
+                _ = yMargin
+
+                fmt.Printf("xMargin: %f   yMargin: %f\n", xMargin, yMargin )
+
+                fmt.Printf("Adding Image to Page ...\n")
+
                 pdf.AddPage()
-                pdf.Image(args[i], xMargin, yMargin, width, height, false, "", 0, "")
+                pdf.Image(args[i], 0, 0, 200, 0, false, "", 0, "")
+
+                fmt.Printf("Adding Image to Page ... DONE\n")
                 
     		} else{
     			// abort
     			fmt.Println("Failed to open file: " + err.Error());
     		}
-
-            fileStr := filepath.Join(".", "pdf/output.pdf")
-            pdfErr := pdf.OutputFileAndClose(fileStr)
-            if pdfErr == nil {
-                    fmt.Println("Successfully generated pdf/output.pdf")
-            } else {
-                    fmt.Println("Error occurred outputting PDF: " + pdfErr.Error())
-            }
-
     	}
+        fileStr := filepath.Join(".", "pdf/output.pdf")
+        pdfErr := pdf.OutputFileAndClose(fileStr)
+        if pdfErr == nil {
+                fmt.Println("Successfully generated pdf/output.pdf")
+        } else {
+                fmt.Println("Error occurred outputting PDF: " + pdfErr.Error())
+        }
     }
 }
